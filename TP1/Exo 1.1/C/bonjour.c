73d247402dffa63dd4bb764b7291d46b527e33bd
cdarmaisin <cedric.darmaisin@teamwork.net>
2024-09-16 09:22:20 +0200

    add ex 1.1 to TP1

---
+++ TP1/Exo 1.1/C/bonjour.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// Driver Code 
+int main() 
+{ 
+    // Print the count of characters 
+    printf("bonjour\n"); 
+  
+    return 0; 
+}
+
+// To run the script : gcc -o bonjour bonjour.c
+// After that to get the result : ./bonjour
---
+++ TP1/Exo 1.1/python/bonjour.py
@@ -0,0 +1,5 @@
+def main():
+    print("bonjour")
+
+if __name__ == "__main__":
+    main()
