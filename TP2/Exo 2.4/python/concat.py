dc8a29560fa46b5e105a77cefde5d7961a4c173b
Feuille2Cedric <106656969+Feuille2Cedric@users.noreply.github.com>
2024-10-06 23:21:13 +0200

    Update concat.py

--- TP2/Exo 2.4/python/concat.py
+++ TP2/Exo 2.4/python/concat.py
@@ -1,13 +1,19 @@
 def concat(chaine1, chaine2):
     # Concaténation de deux chaînes
-    result = chaine1 + chaine2
+    result = chaine1 + chaine2  # Combine les deux chaînes
     return result
 
 def main():
+    # Demander à l'utilisateur de saisir deux chaînes de caractères
     chaine1 = input("Saisissez la première chaîne de caractères : ")
     chaine2 = input("Saisissez la deuxième chaîne de caractères : ")
+    
+    # Concaténer les deux chaînes
     concat_result = concat(chaine1, chaine2)
+    
+    # Afficher le résultat de la concaténation
     print(f"Concaténation des deux chaînes : {concat_result}")
 
+# Point d'entrée du programme
 if __name__ == "__main__":
     main()
