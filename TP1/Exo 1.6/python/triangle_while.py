fe9fb1c7815cfe376f8e747f300a79675749e216
Feuille2Cedric <106656969+Feuille2Cedric@users.noreply.github.com>
2024-10-06 22:53:52 +0200

    Update triangle_while.py

--- TP1/Exo 1.6/python/triangle_while.py
+++ TP1/Exo 1.6/python/triangle_while.py
@@ -2,28 +2,30 @@ def triangle(taille):
     # Validation pour vérifier que la taille est strictement inférieure à 10
     if taille >= 10:
         print("La taille doit être strictement inférieure à 10.")
-        return
+        return  # Sortie de la fonction si la taille n'est pas valide
     
-    i = 1
+    i = 1  # Initialisation de la variable de contrôle pour la boucle des lignes
 
     # Boucle pour dessiner le triangle
-    while i <= taille:
-        line = ""
-        j = 0
-        while j < i:
+    while i <= taille:  # Parcourt chaque ligne du triangle
+        line = ""  # Initialise la chaîne de caractères pour la ligne courante
+        j = 0  # Initialisation de la variable de contrôle pour la boucle des colonnes
+        while j < i:  # Parcourt chaque position dans la ligne courante
             # Affichage des contours avec '*'
-            if j == 0 or j == i - 1 or i == taille:
-                line += "* "
+            if j == 0 or j == i - 1 or i == taille:  # Premier ou dernier caractère de la ligne, ou si c'est la dernière ligne
+                line += "* "  # Ajoute '*' à la chaîne de caractères
             else:
                 # Affichage de l'intérieur avec '#'
-                line += "# "
-            j += 1
-        print(line.strip())
-        i += 1
+                line += "# "  # Ajoute '#' pour les positions intérieures
+            j += 1  # Incrémente la variable de colonne
+        print(line.strip())  # Affiche la ligne courante en retirant les espaces supplémentaires à la fin
+        i += 1  # Incrémente la variable de ligne
 
 def main():
+    # Demande à l'utilisateur d'entrer une taille pour le triangle
     taille = int(input("Entrez une valeur de taille de triangle inférieure à 10 : "))
-    triangle(taille)
+    triangle(taille)  # Appelle la fonction pour dessiner le triangle
 
+# Point d'entrée du programme
 if __name__ == "__main__":
     main()
