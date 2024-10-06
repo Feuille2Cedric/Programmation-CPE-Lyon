7e455c9a8101d5b24610bed939cd5ed1c0d8eefc
Feuille2Cedric <106656969+Feuille2Cedric@users.noreply.github.com>
2024-10-06 23:38:23 +0200

    Create sphere.h

---
+++ TP2/Exo 2.2/C/sphere.h
@@ -0,0 +1,10 @@
+// sphere.h - Fichier d'en-tête contenant les déclarations de fonctions pour la sphère
+
+// Inclusion de la bibliothèque mathématique pour utiliser la constante M_PI
+#include <math.h>
+
+// Fonction pour calculer la surface de la sphère
+float calculer_surface(float rayon);
+
+// Fonction pour calculer le volume de la sphère
+float calculer_volume(float rayon);
