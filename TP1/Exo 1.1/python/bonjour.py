30b0c817f58d8e465e8092ed261a064e64a8296a
Feuille2Cedric <106656969+Feuille2Cedric@users.noreply.github.com>
2024-10-06 22:43:33 +0200

    Update bonjour.py

--- TP1/Exo 1.1/python/bonjour.py
+++ TP1/Exo 1.1/python/bonjour.py
@@ -1,5 +1,8 @@
 def main():
+    # Affiche "bonjour" à l'écran
     print("bonjour")
 
+# Vérifie si le script est exécuté directement (et non importé en tant que module)
 if __name__ == "__main__":
+    # Appelle la fonction main pour exécuter le programme
     main()
