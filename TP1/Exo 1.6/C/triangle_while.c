8f23f958bccb697736237a16e11ff07c73cfb1fa
Feuille2Cedric <106656969+Feuille2Cedric@users.noreply.github.com>
2024-10-06 22:52:43 +0200

    Update triangle_while.c

--- TP1/Exo 1.6/C/triangle_while.c
+++ TP1/Exo 1.6/C/triangle_while.c
@@ -1,41 +1,47 @@
-#include <stdio.h>
+#include <stdio.h> // Inclusion de la bibliothèque standard pour les entrées/sorties
 
+// Fonction pour dessiner un triangle
 void triangle(int taille)
 {
-    int i = 1;
+    int i = 1;  // Initialisation de la variable de contrôle de la boucle pour les lignes
 
+    // Boucle pour chaque ligne du triangle, de 1 à la taille donnée
     while (i <= taille)
     {
-        int j = 0;
+        int j = 0;  // Initialisation de la variable de contrôle de la boucle pour les colonnes
+        // Boucle pour chaque caractère à afficher sur la ligne i
         while (j < i)
         {
+            // Conditions pour savoir quand afficher un "*" ou un "#"
             if (j == 0 || j == i - 1 || i == taille)
             {
-                printf("* ");
+                printf("* ");  // Affiche "*" si on est sur la bordure du triangle ou sur la dernière ligne
             }
             else
             {
-                printf("# ");
+                printf("# ");  // Affiche "#" si on est à l'intérieur du triangle, sans bordure
             }
-            j++;
+            j++;  // Incrémente la variable de colonne
         }
-        printf("\n");
-        i++;
+        printf("\n");  // Nouvelle ligne pour passer à la ligne suivante du triangle
+        i++;  // Incrémente la variable de ligne
     }
 }
 
 int main()
 {
     int taille;
-    
+
     // Demande à l'utilisateur de saisir une taille pour le triangle
     printf("Entrez une valeur pour la taille (strictement inférieure à 10) : ");
+    // Vérifie que l'utilisateur entre un entier valide entre 1 et 9
     while (scanf("%d", &taille) != 1 || taille >= 10 || taille < 1) {
         printf("Veuillez entrer un entier valide entre 1 et 9 : ");
-        while (getchar() != '\n'); // Vider le buffer d'entrée
+        while (getchar() != '\n'); // Vide le buffer d'entrée en cas d'entrée non valide
     }
 
+    // Appelle la fonction triangle pour dessiner le triangle de la taille donnée
     triangle(taille);
 
-    return 0;
+    return 0; // Fin du programme
 }
