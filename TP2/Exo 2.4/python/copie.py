d3776cfe4081389d12e9d23f1840661820fb5103
Feuille2Cedric <106656969+Feuille2Cedric@users.noreply.github.com>
2024-10-06 23:21:41 +0200

    Update copie.py

--- TP2/Exo 2.4/python/copie.py
+++ TP2/Exo 2.4/python/copie.py
@@ -1,14 +1,20 @@
 def copie(source):
     # Copie de la chaîne source dans une nouvelle chaîne
-    destination = ''
+    destination = ''  # Initialisation d'une chaîne vide pour la copie
     for char in source:
-        destination += char
+        destination += char  # Ajoute chaque caractère de la source à la destination
     return destination
 
 def main():
+    # Demande à l'utilisateur de saisir une chaîne de caractères
     chaine = input("Saisissez une chaîne de caractères à copier : ")
+    
+    # Copie la chaîne en utilisant la fonction copie()
     copie_chaine = copie(chaine)
+    
+    # Affiche la copie de la chaîne
     print(f"Copie de la chaîne : {copie_chaine}")
 
+# Point d'entrée du programme
 if __name__ == "__main__":
     main()
