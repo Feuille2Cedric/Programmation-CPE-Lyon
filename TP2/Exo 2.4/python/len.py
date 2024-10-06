f9e2b903fd5c83847d1903b8dfbe3d5a56d81bfc
Feuille2Cedric <106656969+Feuille2Cedric@users.noreply.github.com>
2024-10-06 23:22:06 +0200

    Update len.py

--- TP2/Exo 2.4/python/len.py
+++ TP2/Exo 2.4/python/len.py
@@ -1,13 +1,17 @@
 def longueur(chaine):
     # Calcul de la longueur de la chaîne
-    count = 0
+    count = 0  # Initialise un compteur à 0
     for char in chaine:
-        count += 1
-    return count
+        count += 1  # Incrémente le compteur pour chaque caractère dans la chaîne
+    return count  # Retourne la longueur totale de la chaîne
 
 def main():
+    # Demande à l'utilisateur de saisir une chaîne de caractères
     chaine = input("Saisissez une chaîne de caractères : ")
+    
+    # Affiche la longueur de la chaîne en utilisant la fonction longueur()
     print(f"Longueur de la chaîne : {longueur(chaine)}")
 
+# Point d'entrée du programme
 if __name__ == "__main__":
     main()
