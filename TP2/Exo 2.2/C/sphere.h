3851d189bf1a69dfa9d7c438724f5b13bb29adc9
Feuille2Cedric <106656969+Feuille2Cedric@users.noreply.github.com>
2024-10-06 23:38:44 +0200

    Update sphere.h

--- TP2/Exo 2.2/C/sphere.h
+++ TP2/Exo 2.2/C/sphere.h
@@ -3,6 +3,9 @@
 // Inclusion de la bibliothèque mathématique pour utiliser la constante M_PI
 #include <math.h>
 
+// Définition de la constante M_PI (valeur de π)
+#define M_PI 3.14159265358979323846
+
 // Fonction pour calculer la surface de la sphère
 float calculer_surface(float rayon);
 
