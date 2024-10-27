f49814ce6c6e8d6b25d4a74a6718b2e4faa990b6
Feuille2Cedric <106656969+Feuille2Cedric@users.noreply.github.com>
2024-10-27 22:17:43 +0100

    Update evaluation.h

--- TP5/tp5.1/evaluation.h
+++ TP5/tp5.1/evaluation.h
@@ -2,9 +2,12 @@
 #ifndef EVALUATION_H
 #define EVALUATION_H
 
-#include "parseur.h"
+#include "parseur.h"  // Inclusion de parseur.h pour accéder à la structure Expression
 
-// Fonction d'évaluation de l'expression
+// Déclaration de la fonction d'évaluation de l'expression arithmétique.
+// Cette fonction prend en paramètre un pointeur vers une structure Expression
+// et un pointeur vers un entier pour signaler les erreurs éventuelles.
+// Elle retourne un résultat de type double.
 double evaluate_expression(const Expression *expression, int *error);
 
 #endif // EVALUATION_H
